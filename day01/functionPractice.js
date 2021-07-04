25d4591e5bc1253a556d122ad84e1ff8ad995c9c
Mrmckney <86389090+Mrmckney@users.noreply.github.com>
2021-07-03 22:05:03 -0400

    Add files via upload

---
+++ day01/functionPractice.js
@@ -0,0 +1,18 @@
+// write a function to calculate tax
+// here is just adding some comments
+
+
+function calculateTax(){
+    const taxRate = .07
+    
+    const tesla = 45000
+
+    return (tesla * taxRate).toFixed(2)
+
+}
+    
+
+
+  
+
+console.log( calculateTax() )
